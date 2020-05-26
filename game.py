33070929e185a042e517bacd731235fa8a554d78
Alexander Nedovizin <parkit74@gmail.com>
2020-05-26 23:50:26 +0500

    Второй этап: зачёт!

--- game.py
+++ game.py
@@ -3,17 +3,17 @@
 # pip install -r requirements.txt
 
 from astrobox.space_field import SpaceField
-from vader import VaderDrone
 from kochetov import KochetovDrone
 
 if __name__ == '__main__':
     scene = SpaceField(
         speed=3,
-        asteroids_count=5,
+        asteroids_count=15,
     )
-    # d = [VaderDrone() for _ in range(5)]
+
     k = [KochetovDrone() for _ in range(5)]
     scene.go()
 
 
 # Первый этап: зачёт!
+# Второй этап: зачёт!
