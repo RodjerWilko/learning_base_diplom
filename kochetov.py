4b655bbc02f2eab926ef58837b90059cd01b63df
Максим <ipkochetovmv@gmail.com>
2020-05-28 01:58:00 +0300

    третий этап

--- kochetov.py
+++ kochetov.py
@@ -1,5 +1,4 @@
 from astrobox.core import Drone
-from robogame_engine.geometry import Point
 
 index = 0
 
@@ -7,32 +6,19 @@ index = 0
 class KochetovDrone(Drone):
     asteroid_list = []
     my_drones_list = []
-    center = ''
+    asteroid_pick_list = []
 
     def __init__(self, **kwargs):
         global index
-        self.distance = 0
-        self.empty_distance = 0
-        self.part_load_distance = 0
-        self.full_load_distance = 0
         index += 1
         self.index = index
-        self.type = ''
         super().__init__(**kwargs)
 
     def on_born(self):
-        global index
+
         self.asteroid_list = self.get_sorted_asteroid_list()
         self.my_drones_list.append(self)
-        x = (self.asteroid_list[0][1].coord.x + self.my_mothership.coord.x) / 2
-        y = (self.asteroid_list[0][1].coord.y + self.my_mothership.coord.y) / 2
-        self.center = Point(x, y)
-        if self.index == 1:
-            self.type = 'transport'
-            self.target = self.center
-        else:
-            self.target = self.get_my_asteroid()
-        self.stat(self.target)
+        self.target = self.get_my_asteroid()
         self.move_at(self.target)
 
     def get_sorted_asteroid_list(self):
@@ -40,41 +26,33 @@ class KochetovDrone(Drone):
         a_list = []
         for a in self.asteroids:
             dist = a.distance_to(self.my_mothership)
-            a_list.append([dist, a, 0])
-            a_list.sort(reverse=True)
+            a_list.append([dist, a])
+            a_list.sort(reverse=False)
         return a_list
 
     def get_my_asteroid(self):
-        for i, asteroid in enumerate(self.asteroid_list[::-1]):
-            if asteroid[1].payload == 0:
+        for i, asteroid in enumerate(self.asteroid_list):
+            if asteroid[1].is_empty:
                 continue
-            elif i % 2 == 0 and self.index % 2 == 0:
-                return asteroid[1]
-            elif i % 2 == 0 and self.index % 2 != 0:
+            elif asteroid[1] not in self.asteroid_pick_list:
+                if asteroid[1].payload > 35:  # 35
+                    self.asteroid_pick_list.append(asteroid[1])
+                    return asteroid[1]
+                else:
+                    continue
+            elif i == len(self.asteroid_list) - 1:
+                ast = self.get_any_asteroid()
+                return ast
+
+    def get_any_asteroid(self):
+        for asteroid in self.asteroid_list:
+            if asteroid[1].is_empty:
                 continue
-            elif i % 2 == 1 and self.index % 2 == 1:
-                return asteroid[1]
             else:
-                continue
-
-    def print_stat(self):
-        print(f'№: {self.index}, '
-              f'Вся дистанция: {int(self.distance)}, '
-              f'дальность полета не загруженными:'
-              f' {int((self.empty_distance / self.distance) * 100)} %, '
-              f'дальность полета загруженными полностью: '
-              f'{int((self.full_load_distance / self.distance) * 100)} %, '
-              f'дальность  полета загруженными не полностью: '
-              f'{int((self.part_load_distance / self.distance) * 100)} % ')
-
-    def on_stop_at_point(self, target):
-        if self.near(self.my_drones_list[0]):
-            self.unload_to(self.my_drones_list[0])
-        else:
-            self.stat(self.my_mothership)
-            self.move_at(self.my_mothership)
+                return asteroid[1]
 
     def on_stop_at_asteroid(self, asteroid):
+        self.turn_to(self.my_mothership)
         if asteroid.is_empty:
             self.on_wake_up()
         else:
@@ -82,109 +60,39 @@ class KochetovDrone(Drone):
 
     def on_load_complete(self):
         if self.is_full:
-            if self.index == 4:
-                if self.distance_to(self.my_mothership) > self.distance_to(self.center):
-                    if self.my_drones_list[0].near(self.center):
-                        self.target = self.center
-                        self.stat(self.target)
-                        self.move_at(self.target)
-                    else:
-                        self.stat(self.my_mothership)
-                        self.move_at(self.my_mothership)
-                else:
-                    self.stat(self.my_mothership)
-                    self.move_at(self.my_mothership)
-            else:
-                self.stat(self.my_mothership)
-                self.move_at(self.my_mothership)
-        else:
-            self.on_wake_up()
-
-    def on_stop_at_mothership(self, mothership):
-        if self.type == 'transport':
-            if self.payload == 0:
-                self.print_stat()
-        self.unload_to(mothership)
-
-    def on_unload_complete(self):
-        if self.type == 'transport':  # если дрон транспорт
-            if not all(asteroid.is_empty for asteroid in self.asteroids):  # если не все астероиды пустые
-                for asteroid in self.asteroid_list:
-                    if not asteroid[1].is_empty:
-                        if self.distance_to(asteroid[1]) > self.distance_to(self.center):
-                            self.target = self.center
-                            self.stat(self.center)
-                            self.move_at(self.target)
-                            break
-                        else:
-                            self.target = asteroid[1]
-                            self.stat(asteroid[1])
-                            self.move_at(self.target)
-                            break
-                    else:
-                        continue
-            else:
-                self.print_stat()
-        else:
-            if self.payload == 0:
-                self.on_wake_up()
-            else:
-                if self.target == self.center:
-                    self.on_wake_up()
-                elif self.target.is_empty:
-                    self.on_wake_up()
-                else:
-                    self.stat(self.target)
-                    self.move_at(self.target)
-
-    def stat(self, target):
-        distance = self.distance_to(target)
-        self.distance += distance
-        if self.is_empty:
-            self.empty_distance += distance
-        elif self.is_full:
-            self.full_load_distance += distance
-        else:
-            self.part_load_distance += distance
-
-    def on_wake_up_transport(self):
-        if self.is_full:
-            self.stat(self.my_mothership)
             self.move_at(self.my_mothership)
         else:
-            if all(asteroid.is_empty for asteroid in self.asteroids):
-                self.stat(self.my_mothership)
+            if self.fullness > 0.7:
                 self.move_at(self.my_mothership)
             else:
-                pass
+                self.on_wake_up()
+
+    def on_stop_at_mothership(self, mothership):
+        if self.target:
+            self.turn_to(self.target)
+            self.unload_to(mothership)
+        else:
+            self.unload_to(mothership)
+
+    def on_unload_complete(self):
+        self.on_wake_up()
 
     def on_wake_up(self):
-        if self.type == 'transport':
-            self.on_wake_up_transport()
-        else:
-            if self.target:
-                if self.target == self.center:
-                    self.target = self.get_my_asteroid()
-                    if self.target:
-                        self.stat(self.target)
-                        self.move_at(self.target)
-                    else:
-                        self.print_stat()
-                elif self.target.is_empty:
-                    self.target = self.get_my_asteroid()
-                    if self.target:
-                        self.stat(self.target)
-                        self.move_at(self.target)
-                    else:
-                        self.print_stat()
-                else:
-                    self.stat(self.target)
-                    self.move_at(self.target)
-            else:
+        if self.target:
+            if self.target.is_empty:
                 self.target = self.get_my_asteroid()
                 if self.target:
-                    self.stat(self.target)
                     self.move_at(self.target)
                 else:
-                    self.stat(self.my_mothership)
                     self.move_at(self.my_mothership)
+            else:
+                if self.target.payload < 15:  # 15
+                    self.target = self.get_my_asteroid()
+                    if self.target:
+                        self.move_at(self.target)
+                    else:
+                        self.move_at(self.my_mothership)
+                else:
+                    self.move_at(self.target)
+        else:
+            pass
--- stage_03_harvesters/game.py
+++ stage_03_harvesters/game.py
@@ -4,7 +4,8 @@
 
 from astrobox.space_field import SpaceField
 from stage_03_harvesters.reaper import ReaperDrone
-# TODO тут импортировать своих дронов
+from stage_03_harvesters.driller import DrillerDrone
+from kochetov import KochetovDrone
 from vader import VaderDrone
 
 NUMBER_OF_DRONES = 5
@@ -14,9 +15,9 @@ if __name__ == '__main__':
         speed=5,
         asteroids_count=20,
     )
-    # TODO создать их
-    team_1 = [VaderDrone() for _ in range(NUMBER_OF_DRONES)]
-    # TODO и побороть противников!
-    team_2 = [ReaperDrone() for _ in range(NUMBER_OF_DRONES)]
+    my_team = [KochetovDrone() for _ in range(NUMBER_OF_DRONES)]
+    # team_1 = [VaderDrone() for _ in range(NUMBER_OF_DRONES)]
+    team_3 = [DrillerDrone() for _ in range(NUMBER_OF_DRONES)]
+    # team_2 = [ReaperDrone() for _ in range(NUMBER_OF_DRONES)]
     scene.go()
 
